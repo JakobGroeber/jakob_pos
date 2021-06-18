3fc1c139bc65d1005b769ca64e0ab49e19b4d2f2
Jakob Gröber <77675480+JakobGroeber@users.noreply.github.com>
2021-06-18 11:21:13 +0200

    Update MainAppl.java

--- MyTeam/src/Main/MainAppl.java
+++ MyTeam/src/Main/MainAppl.java
@@ -5,7 +5,7 @@ import pkgData.Player;
 public class MainAppl {
 
 	public static void main(String[] args) {
-		Player player = new Player(2, "Dragovic", "Defense");
+		Player player = new Player(4, "Hinteregger", "Defense");
 		System.out.println("== " +player);
 	}
 
